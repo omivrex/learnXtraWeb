ae0aa4e71928768704485a6c0fe6fb86f589b016
RexOmiv <omivrex@gmail.com>
2022-05-11 08:13:03 +0100

    added eact router

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import logo from './logo.png';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import AboutJ from './components/AboutJ.compoenent';
 import Navbar from './components/Navbar.component';
+import Home from './components/Home.component';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar/>
-      <section className='banner'>
-        <div className='bannerContent'>
-          <h1>LEARNXTRA</h1>
-          <h3>Assurance In Your A-Level Exam</h3>
-          <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui molestiae magnam nihil sunt quos fugit assumenda atque et hic, soluta aperiam? Quasi temporibus facere debitis officia perferendis sunt provident placeat?</h4>
-          <span>Get The App</span>
-        </div>
-        <img id='logo' src={logo} alt="App logo" />
-      </section>
-
-      <section>
-        <div>Our Services</div>
-        <div>About JUPEB</div>
-        <div>Recent Activities</div>
-        <div>Resources</div>
-      </section>
-    </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/aboutJUPEB' element={<AboutJ/>}/>
+      </Routes>
+    </Router>
   );
 }
 
