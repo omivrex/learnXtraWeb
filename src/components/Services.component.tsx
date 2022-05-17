2e247f949a4ae573060a6dcc34c5f90574d3dba3
RexOmiv <omivrex@gmail.com>
2022-05-17 01:57:52 +0100

    fixed bug with assing image and modified styles in services page

--- src/components/Services.component.tsx
+++ src/components/Services.component.tsx
@@ -2,39 +2,43 @@ import React, {ReactElement} from "react";
 import { GiTeacher } from 'react-icons/gi';
 import tutorial from "../assets/tutorial.jpg";
 import assign from "../assets/assign.jpg";
+import research from "../assets/research.jpg";
+import graduate from "../assets/graduate.jpg";
 
 const Services = ():ReactElement => {
   return (
     <div style={{...styles.container, ...{flexDirection: 'column'}}}>
         <div style={{... styles.header, ...{textAlign: 'center', flexDirection: 'column', justifyContent: 'center'}}}>
-          <h1 style={{borderBottom: '2px solid #eee'}}>What We Do</h1>
+          <h1 style={styles.h1}>What We Do</h1>
           <p style={{width: '70%', margin: '0 auto'}}>
             At LearnXtra, we believe that education is the key that unlocks the door to numerous opportunities in life. Thus, we want everyone to find learning easy and interesting.
             We provide a range of educational support services for school leavers, undergraduate students, postgraduate students and tertiary institutions across Nigeria and beyond. 
           </p>
-        </div>
-
-        <div style={{... styles.header, ...{flexDirection: 'column', textAlign: 'center', justifyContent: 'center'}}}>
-          <h1 style={{borderBottom: '2px solid #eee', textAlign: 'center'}}>Our solutions include:</h1>
-          <ul style={styles.ul}>
-            <li style={styles.li}>
-              <img width={'90%'} src={tutorial}/>
-              Tutorials
-            </li>
-            <li style={styles.li}>
-              <img width={'90%'} src={assign  }/>
-              Assignments
-            </li>
-            <li style={styles.li}>
-              Academic research work <br /> (term papers, project, thesis)
-            </li>
-            <li style={styles.li}>
-              Virtual graduate assistant
-            </li>
-          </ul>
           <p>
             Our promise is quality and effective solutions, on-time delivery, and excellent customer service.
           </p>
+        </div>
+
+        <div style={{... styles.header, ...{flexDirection: 'column', textAlign: 'center', justifyContent: 'center'}}}>
+          <h1 style={styles.h1}>Our solutions include:</h1>
+          <div style={styles.ul}>
+            <div style={styles.li}>
+              <img width={'90%'} src={tutorial}/>
+              Tutorials
+            </div>
+            <div style={styles.li}>
+              <img width={'90%'} src={assign}/>
+              Assignments
+            </div>
+            <div style={styles.li}>
+              <img width={'90%'} src={research}/>
+              Academic research work <br /> (term papers, project, thesis)
+            </div>
+            <div style={styles.li}>
+              <img width={'90%'} src={graduate}/>
+              Virtual graduate assistant
+            </div>
+          </div>
         </div>
 
         
@@ -45,23 +49,31 @@ const Services = ():ReactElement => {
 const styles = {
   container: {
     margin: '3rem auto',
-    width: '90%',
+    marginBottom: 0,
+    width: '100%',
     display: 'flex',
+    backgroundColor: '#531d5c',
+    position: 'relative'as any,
+    height: 'fit-content',
     alignItems: 'center'
   },
 
   header: {
-    backgroundColor: '#531d5c',
     color: '#eee',
     width: '100%',
-    height: '21rem',
+    padding: '10rem 0',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
+  },
+
+  h1: {
+    textDecoration: 'underline'
   },
 
   ul: {
-    display: 'flex', 
+    display: 'flex',
+    flexWrap: 'wrap' as any,
     width: '100%',
     flexDirection: 'row' as any,
     aligItems: 'center' as any,
@@ -72,10 +84,11 @@ const styles = {
     listStyle: 'none',
     width: '15rem',
     height: '15rem',
+    margin: 'auto',
     // backgroundColor: '#eee',
     display: 'flex',
     color: '#eee',
-    justifyContent: 'sapce-arround',
+    justifyContent: 'space-around',
     flexDirection: 'column' as any,
     alignItems: 'center'
   }
